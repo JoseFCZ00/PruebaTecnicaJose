3af30f419db2b5aa5e8d0393682ec0edba9624b5
JoseFCZ00 <jose01fernandp@gmail.com>
2024-06-20 18:07:39 -0600

    Add files via upload
    Aqui esta el Index, es algo sencillo con 2 botones, uno que llevara a una pagina para hacer registro de usuarios y el otro es para ver los usuarios ya registrados.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inicio - CRUD PHP</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+        }
+
+        .container {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Inicio - Prueba Tecnica CRUD</h1>
+        <div class="menu">
+            <a href="crear_registro.php" class="btn">Crear Nuevo Registro</a>
+            <a href="leer.php" class="btn">Ver Registros</a>
+        </div>
+    </div>
+</body>
+</html>
